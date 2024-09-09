e9872a1ad322cdab05c5cba3a0a65b2f9b2fbd82
jordkh <65198468+jordkh@users.noreply.github.com>
2024-09-09 07:51:57 -0700

    update domain to libbyapp.com (#2)

--- odmpy/libby.py
+++ odmpy/libby.py
@@ -290,7 +290,7 @@ class LibbyClient(object):
             libby_session.mount(prefix, adapter)
         self.libby_session = libby_session
         self.user_agent = kwargs.pop("user_agent", USER_AGENT)
-        self.api_base = "https://sentry-read.svc.overdrive.com/"
+        self.api_base = "https://sentry.libbyapp.com/"
 
     @staticmethod
     def is_valid_sync_code(code: str) -> bool:
